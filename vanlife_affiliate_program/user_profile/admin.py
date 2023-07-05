63f5d736bff4daf0934771aad7921eee3904827a
Tadas Zupka <tadas.zupka@gmail.com>
2023-07-05 10:05:39 +0300

    project reset

--- vanlife_affiliate_program/user_profile/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . models import ProfileIcon, Profile, ProfileReason
-
-class ProfileIconAdmin(admin.ModelAdmin):
-    list_display = ('__str__',)
-
-admin.site.register(ProfileIcon, ProfileIconAdmin)
-admin.site.register(Profile)
-admin.site.register(ProfileReason)
--- vanlife_affiliate_program/user_profile/forms.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth import get_user_model
-from django import forms
-from . import models
-
-User = get_user_model()
-
