956da4a8324f22c2ba850cf36ae52b33f6575a3c
Tadas Zupka <tadas.zupka@gmail.com>
2023-07-05 18:28:15 +0300

    templates, styles

--- vanlife_affiliate_program/conversions/urls.py
+++ vanlife_affiliate_program/conversions/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from . views import home
+from . views import home, about, affiliate, van_list, rv_list
 
 urlpatterns = [
     path('', home, name="home"),
+    path('about/', about, name="about"),
+    path("affiliate/", affiliate, name="aff_guide"),
+    path("vans/", van_list, name="van_list"),
+    path("rvs/", rv_list, name="rv_list"),
 ]
--- vanlife_affiliate_program/conversions/views.py
+++ vanlife_affiliate_program/conversions/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'conversions/home.html')
+
+def about(request):
+    return render(request, 'conversions/about.html')
+
+def affiliate(request):
+    return render(request, 'conversions/aff_guide.html')
+
+def van_list(request):
+    return render(request, 'conversions/van_list.html')
+
+def rv_list(request):
+    return render(request, 'conversions/rv_list.html')
