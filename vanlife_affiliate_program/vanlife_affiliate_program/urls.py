0828e61c4b2a959fc3af8a8a0322f2c9205b0a3c
Tadas Zupka <tadas.zupka@gmail.com>
2023-07-11 17:07:00 +0300

    register/login/logout views/templates

--- vanlife_affiliate_program/vanlife_affiliate_program/urls.py
+++ vanlife_affiliate_program/vanlife_affiliate_program/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('conversions.urls')),
     path('', include('blog.urls')),
+    path('', include('users.urls')),
     path('tinymce/', include('tinymce.urls')),
 ]  + (static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)\
      + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
